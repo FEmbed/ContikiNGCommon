50094145bae8fc9d299e0da873631cfa6fbc2186
Gene Kong <gyx_edu@qq.com>
2020-09-05 13:27:35 +0800

    feat: add freq center config with sdkconfig.

--- include/contiki_ng_conf.h
+++ include/contiki_ng_conf.h
@@ -25,6 +25,41 @@
 #define MAC_CONF_WITH_CSMA      (1)
 #endif
 
-// defautl contiki-ng string convert.
+////////////////////////////////////////////////////////////////////////////////
+/// defautl contiki-ng string convert.
+////////////////////////////////////////////////////////////////////////////////
 #define  CONTIKI_TARGET_STRING      CONFIG_CONTIKI_TARGET
 #define  CONTIKI_BOARD_STRING       CONFIG_CONTIKI_BOARD
+
+////////////////////////////////////////////////////////////////////////////////
+/// override RF center frequency
+////////////////////////////////////////////////////////////////////////////////
+#ifdef  CONFIG_RF_CENTER_FRQ_169
+#define DOT_15_4G_CONF_FREQ_BAND_ID 0
+#elif defined(CONFIG_RF_CENTER_FRQ_450)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 1
+#elif defined(CONFIG_RF_CENTER_FRQ_470)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 2
+#elif defined(CONFIG_RF_CENTER_FRQ_780)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 3
+#elif defined(CONFIG_RF_CENTER_FRQ_863)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 4
+#elif defined(CONFIG_RF_CENTER_FRQ_896)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 5
+#elif defined(CONFIG_RF_CENTER_FRQ_901)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 6
+#elif defined(CONFIG_RF_CENTER_FRQ_915)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 7
+#elif defined(CONFIG_RF_CENTER_FRQ_917)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 8
+#elif defined(CONFIG_RF_CENTER_FRQ_920)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 9
+#elif defined(CONFIG_RF_CENTER_FRQ_928)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 10
+#elif defined(CONFIG_RF_CENTER_FRQ_950)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 11
+#elif defined(CONFIG_RF_CENTER_FRQ_1427)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 12
+#elif defined(CONFIG_RF_CENTER_FRQ_2450)
+#define DOT_15_4G_CONF_FREQ_BAND_ID 13
+#endif
